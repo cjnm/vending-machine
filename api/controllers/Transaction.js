70debf967a4a760000c3fe9ce234c80c337bbc79
Guest <guest@pop-os.localdomain>
2022-01-24 01:41:20 +0545

    return api implemented

--- api/controllers/Transaction.js
+++ api/controllers/Transaction.js
@@ -3,39 +3,57 @@ const MoneyModel = require('../models/Money');
 
 exports.buy = async (orders, coin) => {
     if (!Array.isArray(orders)) {
-        return { status: 400, error: 'Invalid argument supplied for purchase.', purchase: null, refund: coin };
+        return { status: 400, error: 'Invalid argument supplied for purchase.', products: null, refund: coin };
     }
 
     if (!await areProductsAvailable(orders)) {
-        return { status: 400, error: 'Supplied products does not exist in inventory.', purchase: null, refund: coin };
+        return { status: 400, error: 'Supplied products does not exist in inventory.', products: null, refund: coin };
     }
 
     let cost = await calculateCost(orders);
     let refundAmount = coin - cost;
 
     if (refundAmount < 0) {
-        return { status: 400, error: 'Insufficient coin insetred.', purchase: null, refund: coin };
+        return { status: 400, error: 'Insufficient coin insetred.', products: null, refund: coin };
     }
 
     if (!await isInventoryAvailable(orders)) {
-        return { status: 400, error: 'Insufficient inventory available.', purchase: null, refund: coin };
+        return { status: 400, error: 'Insufficient inventory available.', products: null, refund: coin };
     }
 
     // Initializing purchase.
     await updateInventory(orders);
     await updateMoney(cost);
-    return { status: 200, error: null, purchase: orders, refund: refundAmount };
+    return { status: 200, error: null, products: orders, refund: refundAmount };
 }
 
 exports.return = async (orders) => {
-    //To be implemented
+    if (!Array.isArray(orders)) {
+        return { status: 400, error: 'Invalid argument supplied for return.', products: null, refund: null };
+    }
+
+    if (!await areProductsAvailable(orders)) {
+        return { status: 400, error: 'Supplied products was not sold from this vending machine.', products: orders, refund: null };
+    }
+
+    if (!await isInventoryRefundable(orders)) {
+        return { status: 400, error: 'Cannot return more products than total sold', products: orders, refund: null };
+    }
+
+    let refundAmount = await calculateCost(orders);
+
+    // Initializing purchase.
+    await updateInventory(orders, true);
+    await updateMoney(-refundAmount);
+    return { status: 200, error: null, products: null, refund: refundAmount };
+
 }
 
-async function updateInventory(orders) {
+async function updateInventory(orders, refund = false) {
     let newInventory = 0;
     orders.forEach(async (order) => {
         InventoriesModel.findOne({ name: order.item }, async (item) => {
-            newInventory = item.quantity;
+            newInventory = refund ? item.quantity + order.quantity : item.quantity - order.quantity;
             await InventoriesModel.findOneAndUpdate({ _id: item._id }, { quantity: newInventory });
         });
     });
@@ -67,6 +85,18 @@ async function isInventoryAvailable(orders) {
         });
     });
     return availibility;
+}
+
+async function isInventoryRefundable(orders) {
+    let refundable = true;
+    orders.forEach(async (order) => {
+        await InventoriesModel.findOne({ name: order.item }, function (inventory) {
+            if ((10 - inventory.quantity) < order.quantity) {
+                refundable = false;
+            }
+        });
+    });
+    return refundable;
 }
 
 async function areProductsAvailable(orders) {
